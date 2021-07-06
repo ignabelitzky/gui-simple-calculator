491a3fba2489913967a980a93d840cefcb509f02
Ignacio Belitzky <ignabelitzky@gmail.com>
2021-07-06 18:11:08 -0300

    Functionality was added to the buttons

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -6,10 +6,147 @@ Calculator::Calculator(QWidget *parent)
     , ui(new Ui::Calculator)
 {
     ui->setupUi(this);
+    display = ui->display_lineEdit;
+    value1 = 0;
+    value2 = 0;
+    operation = '\0';
+    result = 0;
 }
 
 Calculator::~Calculator()
 {
     delete ui;
+}
+
+
+void Calculator::on_zero_btn_clicked()
+{
+    QString previousText = display->text();
+    display->setText(previousText + "0");
+}
+
+
+void Calculator::on_one_btn_clicked()
+{
+    QString previousText = display->text();
+    display->setText(previousText + "1");
+}
+
+
+void Calculator::on_two_btn_clicked()
+{
+    QString previousText = display->text();
+    display->setText(previousText + "2");
+}
+
+
+void Calculator::on_three_btn_clicked()
+{
+   QString previousText = display->text();
+   display->setText(previousText + "3");
+}
+
+
+void Calculator::on_four_btn_clicked()
+{
+   QString previousText = display->text();
+   display->setText(previousText + "4");
+}
+
+
+void Calculator::on_five_btn_clicked()
+{
+   QString previousText = display->text();
+   display->setText(previousText + "5");
+
+}
+
+
+void Calculator::on_six_btn_clicked()
+{
+   QString previousText = display->text();
+   display->setText(previousText + "6");
+}
+
+
+void Calculator::on_seven_btn_clicked()
+{
+   QString previousText = display->text();
+   display->setText(previousText + "7");
+}
+
+
+void Calculator::on_eight_btn_clicked()
+{
+   QString previousText = display->text();
+   display->setText(previousText + "8");
+}
+
+
+void Calculator::on_nine_btn_clicked()
+{
+   QString previousText = display->text();
+   display->setText(previousText + "9");
+}
+
+
+void Calculator::on_clear_btn_clicked()
+{
+   display->setText("");
+}
+
+void Calculator::on_divide_btn_clicked()
+{
+    value1 = display->text().toDouble();
+    operation = '/';
+    display->setText("");
+}
+
+
+void Calculator::on_product_btn_clicked()
+{
+    value1 = display->text().toDouble();
+    operation = '*';
+    display->setText("");
+}
+
+
+void Calculator::on_subtract_btn_clicked()
+{
+    value1 = display->text().toDouble();
+    operation = '-';
+    display->setText("");
+}
+
+
+void Calculator::on_sum_btn_clicked()
+{
+    value1 = display->text().toDouble();
+    operation = '+';
+    display->setText("");
+}
+
+
+void Calculator::on_equal_btn_clicked()
+{
+    value2 = display->text().toDouble();
+    switch(operation) {
+    case '+':
+        result = value1 + value2;
+        break;
+    case '-':
+        result = value1 - value2;
+        break;
+    case '*':
+        result = value1 * value2;
+        break;
+    case '/':
+        result = value1 / value2;
+        break;
+    default:
+        qDebug() << "Invalid operation";
+        break;
+    }
+    display->setText(QString::number(result));
 }
 
--- calculator/calculator.h
+++ calculator/calculator.h
@@ -2,6 +2,7 @@
 #define CALCULATOR_H
 
 #include <QMainWindow>
+#include <QLineEdit>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Calculator; }
@@ -15,7 +16,46 @@ public:
     Calculator(QWidget *parent = nullptr);
     ~Calculator();
 
+private slots:
+    void on_zero_btn_clicked();
+
+    void on_one_btn_clicked();
+
+    void on_two_btn_clicked();
+
+    void on_three_btn_clicked();
+
+    void on_four_btn_clicked();
+
+    void on_five_btn_clicked();
+
+    void on_six_btn_clicked();
+
+    void on_seven_btn_clicked();
+
+    void on_eight_btn_clicked();
+
+    void on_nine_btn_clicked();
+
+    void on_clear_btn_clicked();
+
+    void on_divide_btn_clicked();
+
+    void on_product_btn_clicked();
+
+    void on_subtract_btn_clicked();
+
+    void on_sum_btn_clicked();
+
+    void on_equal_btn_clicked();
+
 private:
     Ui::Calculator *ui;
+    QLineEdit *display;
+    double value1;
+    double value2;
+    double result;
+    char operation;
+
 };
 #endif // CALCULATOR_H
