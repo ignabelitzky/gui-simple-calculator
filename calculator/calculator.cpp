f4fd3ddb185495a89704a05764a258b381049738
Ignacio Belitzky <ignabelitzky@gmail.com>
2021-07-07 22:13:09 -0300

    Minor changes in the UI

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -45,63 +45,65 @@ void Calculator::on_two_btn_clicked()
 
 void Calculator::on_three_btn_clicked()
 {
-   QString previousText = display->text();
-   display->setText(previousText + "3");
+    QString previousText = display->text();
+    display->setText(previousText + "3");
 }
 
 
 void Calculator::on_four_btn_clicked()
 {
-   QString previousText = display->text();
-   display->setText(previousText + "4");
+    QString previousText = display->text();
+    display->setText(previousText + "4");
 }
 
 
 void Calculator::on_five_btn_clicked()
 {
-   QString previousText = display->text();
-   display->setText(previousText + "5");
-
+    QString previousText = display->text();
+    display->setText(previousText + "5");
 }
 
 
 void Calculator::on_six_btn_clicked()
 {
-   QString previousText = display->text();
-   display->setText(previousText + "6");
+    QString previousText = display->text();
+    display->setText(previousText + "6");
 }
 
 
 void Calculator::on_seven_btn_clicked()
 {
-   QString previousText = display->text();
-   display->setText(previousText + "7");
+    QString previousText = display->text();
+    display->setText(previousText + "7");
 }
 
 
 void Calculator::on_eight_btn_clicked()
 {
-   QString previousText = display->text();
-   display->setText(previousText + "8");
+    QString previousText = display->text();
+    display->setText(previousText + "8");
 }
 
 
 void Calculator::on_nine_btn_clicked()
 {
-   QString previousText = display->text();
-   display->setText(previousText + "9");
+    QString previousText = display->text();
+    display->setText(previousText + "9");
 }
 
 void Calculator::on_comma_btn_clicked()
 {
-   QString previousText = display->text();
-   display->setText(previousText + ".");
+    QString previousText = display->text();
+    display->setText(previousText + ".");
 }
 
 void Calculator::on_clear_btn_clicked()
 {
-   display->setText("");
-   statusLabel->setText("");
+    firstValue = 0;
+    secondValue = 0;
+    result = 0;
+    display->setText("");
+    statusLabel->setText("");
 }
 
 void Calculator::on_divide_btn_clicked()
